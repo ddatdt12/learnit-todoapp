3df343203049c8ac93e2d19861a8b13ff6ac4cc9
Thanh Dat <ddatdt12@gmail.com>
2021-10-11 19:53:57 +0700

    Fix origin cross with cookie

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -7,14 +7,19 @@ const catchAsync = require('../utils/catchAsync');
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
 
-  res.cookie('jwt', token, {
+  // Default for localhost
+  let cookieOptions = {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
     ),
     httpOnly: true,
     secure: false,
-    // secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
-  });
+  };
+  if(process.env.NODE_ENV === 'production') {
+    cookieOptions.sameSite='none',
+    cookieOptions.secure=true,
+  }
+  res.cookie('jwt', token, cookieOptions);
 
   // Remove password from output
   user.password = undefined;
